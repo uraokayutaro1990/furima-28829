cc2b7ad50f0f09a4328cf2cb246ac86bc99c08f5
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-12 15:53:24 +0900

    ニックネームの単体テストを削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,10 +21,6 @@ describe User do
     end
 
     context '新規登録がうまくいかないとき' do
-      it "nicknameが空だと登録できない" do
-        @user.nickname = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       it "nameが空だと登録できない" do
         @user.name = ''
         @user.valid?
