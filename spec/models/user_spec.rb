3bbc86e74f9c21303f453f1d2f489ce06b3ca29f
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-12 21:57:25 +0900

    51-54修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,10 +48,10 @@ describe User do
         @user.password_confirmation = "00000"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
-        it "passwordが半角英数字混合出ないと登録できない" do
-          @user.password = "111aaa"
-          @user.valid?
-          expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      it "passwordが半角英数字混合出ないと登録できない" do
+        @user.password = "111aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
       it "passwordが存在してもpassword_confirmationが空では登録できない" do
         @user.password_confirmation = ""
