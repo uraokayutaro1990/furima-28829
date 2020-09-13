b6f6505f5831fe5d4708f34232fba755f78eb26b
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-13 10:31:43 +0900

    コメントアウトコード削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
   
   resources :items
 
-  
-  # devise_scope :user do
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
