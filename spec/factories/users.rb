2c1e2ffec6159bac6cb02553dde4de75b5328e38
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-13 11:42:45 +0900

    fakerでダミーを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :user do
+    nickname {Faker::Lorem.sentence}
+    email {Faker::Lorem.sentence}
+   password  {Faker::Lorem.sentence}
+   name  {Faker::Lorem.sentence}
+   save  {Faker::Lorem.sentence}
+   password_confirmation  {Faker::Lorem.sentence}
+   family_name {Faker::Lorem.sentence}
+   first_name {Faker::Lorem.sentence}
+   family_name_kana {Faker::Lorem.sentence}
+   first_name_kana {Faker::Lorem.sentence}
+   birthday
     
   end
 end
+
+
+
+
+  
