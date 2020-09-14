a4d0f5206f1b60e2a719e508a93ef232ecdc0791
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-14 20:00:53 +0900

    クリエイト新規ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,23 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-  params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-end
+  params.require(:item).permit(:content, :image,name,price).merge(user_id: current_user.id)
+  end
+
+  def create
+    @item = item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
+    private
+
+  def item_params
+    params.require(:room).permit(:name)
+  end
+
+  end
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   with_options presence: true do
     
-
-      
-      zyouken3 =                 /\A[0-9]+\z/
-      
-      validates  :
+      validates  :image, presence: true, 
       validates  :name,  presence: true, format: {with: maxinum: 40},
       validates  :text,  presence: true format: {with: maxinum: 1000},
       validates  :category,  presence: true
@@ -15,13 +11,10 @@ class Item < ApplicationRecord
       validates  :postage,  presence: true
       validates  :prefecture,  presence: true
       validates  :delivery_time,  presence: true
-  
-
-      validates  :price,  presence: true, format: {with: zyouuken3},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,
+      validates  :price,  presence: true, numericality: {with: /\A[0-9]+\z/},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,
         greater_than: 300, less_than: 10000000
       }
-
-
+      
   end
 end
 
