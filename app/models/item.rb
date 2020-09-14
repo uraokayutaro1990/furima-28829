aa3bb43569cd21f01b3147c709555f3caf1b1068
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-15 01:43:40 +0900

    カテゴリ選択のビューファイル完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,14 @@ class Item < ApplicationRecord
     validates  :image 
     # validates  :name, format: {with: maxinum:40}
     # validates  :text,   format: {with: maxinum:1000}
-    validates  :name, length: {maximum:40}
-    validates  :text,   length: {maximum:1000}
-    validates  :category 
-    validates  :condition
-    validates  :postage 
-    validates  :prefecture 
-    validates  :delivery_time
-    validates  :price, numericality: {with: /\A[0-9]+\z/},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,greater_than: 300, less_than: 10000000}
+    # validates  :name, length: {maximum:40}
+    # validates  :text,   length: {maximum:1000}
+    # validates  :category 
+    # validates  :condition
+    # validates  :postage 
+    # validates  :prefecture 
+    # validates  :delivery_time
+    # validates  :price, numericality: {with: /\A[0-9]+\z/},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,greater_than: 300, less_than: 10000000}
   end
 end
 
