7fc03fd51c78d44d3889e6507be28a1ab3b2415c
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-15 19:01:22 +0900

    javascript完了

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,15 +1,12 @@
 function item() { 
- const price = document.getElementById("item-price");
- price.addEventListener("input", (e) => {
-const formData = new FormData(document.getElementById("form")); 
+  const price = document.getElementById("item-price");
+  price.addEventListener("input", () => {
+    const value = price.value 
+    const profit = value * 0.9 
+    const fee = value * 0.1
+    document.getElementById("add-tax-price").innerHTML=fee
+    document.getElementById("profit").innerHTML=profit
 
-
-
-
- 
+  })
 }
 window.addEventListener("load", item);
-
-// 1000と入ったら、１０００を取得する。
-// 取得した１０００をつかって、利益と手数料を計算する。
-// 計算結果を表示する。
