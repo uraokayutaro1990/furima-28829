b656dfb07905d43c2faf645bcab01ce8a561c080
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-16 01:00:47 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,17 +7,27 @@ RSpec.describe Item, type: :model do
 
 describe '商品情報を入力' do
   context '出品がうまくいくとき' do
-    it "imageとname、textとが存在すれば登録できる" do
+    it "全ての値が存在する時、新規登録できる" do
       expect(@item).to be_valid
     end
   end
 end
 
 context '情報がないと出品できない' do
-    it "画像を保存" do
-      @item.image= nil
+    it "画像1枚必須" do
+      @item.image= "0"
       @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
+      expect(@item.errors.full_messages).to include("Image have to have one")
+    end
+    it "商品名" do
+      @item.name = 'あa1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it "商品説明" do
+      @item.text = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("text can't be blank")
     end
     it "カテゴリ" do
       @item.category = ''
@@ -39,10 +49,29 @@ context '情報がないと出品できない' do
       @item.valid?
         expect(@item.errors.full_messages).to include("Day can't be blank")
     end
+    it "値段" do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it "passwordが半角英数字混合出ないと登録できない" do
+      @item.price = "aaa111"
+      @.valitemid?
+      expect(@item.errors.full_messages).to include ("Price confirmation doesn't match Price")
+    end
+    it "販売価格が半角英数字混合出ないと登録できない" do
+      @item.Price = "aaa111"
+      @item.valid?
+      expect(@user.errors.full_messages).to include ("Price confirmation doesn't match Price")
+    end
+    it "販売価格が￥300~￥9,999,999の間であること" do
+      @item.user = Number.between(from: 300 ,to: 9999999)} #=> 1968353479   
+      @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
     it "ユーザー" do
       @item.user = nil
       @item.valid?
-        expect(@item.errors.full_messages).to include("User can't be blank")
+        expect(@item.errors.full_messages).to include("User can't be blank")  
     end
   end
 end
