da3f3de1b9edd12cf84a931d5a29910501c6bb5c
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-17 01:48:37 +0900

    削除機能完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    @item = Item.find(params[:id]) 
+    @item.destroy
+    redirect_to root_path
+  end
+
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
