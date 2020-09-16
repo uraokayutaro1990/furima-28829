3d769f1c7e6e55203cb8945a8b76d15b6eb6f443
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-16 21:47:27 +0900

    修正間違えたので。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,13 +29,6 @@ class ItemsController < ApplicationController
   end
 #1つだけ表示させる、クリックしたものを
 
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
-  end
-end
-#ログインしたユーザーのみ一覧に飛べる
-
     private
     #クラス内のみアクセス可能
 
