ce83f4df48d33d2545c69f8a7d8da00aeee09ccb
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-17 20:22:44 +0900

    修正完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   end 
 
   def create
-    @order = Order.new(order_params[:user_id,:item_id])
+    @order = Order.new(order_params)
     if @order.valid?
       pay_item
       @order.save
