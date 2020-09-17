ad619aa286bf655359ecbe132da2881997dea6bf
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-17 15:22:49 +0900

    誤ってそのままにしていたので、削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id :current_user_id])
   end 
 end
+
 
 #orders テーブルのレコードに必要な情報は「商品」と「購入者」である。
 #購入するのページに飛べるように,index.ok
 #アカウントをもう一つ作って確認
+#購入ページにはいけた
+#ログインユーザーのみ 購入ページにアクセスできる設定にする。
