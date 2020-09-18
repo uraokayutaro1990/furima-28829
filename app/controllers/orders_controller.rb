9da6fca08ac7af6b452edb2b86417b79b0e5f11e
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-18 10:52:54 +0900

    追記、order_id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,20 +8,25 @@ class OrdersController < ApplicationController
 
   def create 
     @item = Item.find(params[:item_id])
-    @address_params = {
+    buy_params = { 
+      item_id: order_params[:item_id],
+      user_id: order_params[:user_id]
+    }
+    @order = Order.new(buy_params)
+    address_params = {
       postal_code: order_params[:postal_code],
-      prefecture_id: order_params[:prefecture_id],
-      ctiy:order_params[:city],
-      addresses: order_params[:address],
+      prefecture: order_params[:prefecture_id],
+      city:order_params[:city],
+      addresses: order_params[:addresses],
       building: order_params[:building],
       phone_number: order_params[:phone_number] 
     }
-    @order = Order.new(order_params)
-    @address = Address.new(order_params)
+    @address = Address.new(address_params)
     binding.pry
     if @order.valid?
        pay_item
        @order.save
+       @address.order_id = @order.id
        @address.save
       return redirect_to root_path
     else
@@ -29,11 +34,10 @@ class OrdersController < ApplicationController
     end
   end
 
-
 private
 
 def order_params
-  params.permit(:price, :token,:item_id , :authenticity_token, :postal_code, :prefecture_id, :ctiy, :addresses, :building, :phone_number ).merge(user_id: current_user.id )
+  params.permit(:price, :token,:item_id , :authenticity_token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number ).merge(user_id: current_user.id )
 end
 
 def pay_item
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_145205) do
+ActiveRecord::Schema.define(version: 2020_09_18_014453) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_145205) do
     t.string "phone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "order_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
