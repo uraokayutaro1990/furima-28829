bb253c17a7eb17a35a163242b8f1309a921c93a4
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-18 11:51:25 +0900

    スペルミス

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!,only: [:index]
-  before_ation  :set_item,only: [:index,:create] 
+  before_action  :set_item,only: [:index,:create] 
   def index
     @order = Order.new
     @address = Address.new 
